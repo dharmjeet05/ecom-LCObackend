4f6e702048af3e0aca18221d1a887156aad7400a
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-11-26 22:05:48 +0530

    All packages configuration done in app.js

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const express = require("express");
 require("dotenv").config();
 
+const express = require("express");
 const app = express();
+
+// Packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+// for swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocument = yaml.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookies and file upload middlewares
+app.use(cookieParser());
+app.use(fileUpload());
+
+// morgan middlewares
+app.use(morgan("tiny"));
 
 // routes
 const homeRoute = require("./routes/homeRoute");
 
-// router middleware
 app.use(`/api/${process.env.VERSION}`, homeRoute);
 
 // export app js
