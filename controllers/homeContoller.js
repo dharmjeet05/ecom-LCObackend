1427dedd2cfa213593990a2b86e0b30c2d5052ee
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-11-26 22:19:20 +0530

    TheBigPromise

--- controllers/homeContoller.js
+++ controllers/homeContoller.js
@@ -1,6 +1,9 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middlewares/bigPromise");
+
+exports.home = BigPromise(async (req, res) => {
+    // const db = await somethin()
     res.status(200).json({
         success: true,
         greeting: "Hello from API",
     });
-};
+});
