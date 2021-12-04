9bc02a38228103c233a524baaf3cada809bdd704
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-12-04 19:47:58 +0530

    LogOut is all Done

--- controllers/userController.js
+++ controllers/userController.js
@@ -103,3 +103,15 @@ exports.login = bigPromise(async (req, res, next) => {
         user,
     });
 });
+
+exports.logout = bigPromise(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logout success",
+    });
+});
